b7c48077aeed50a12edace15c8c5fcc16c0ef0bb
yanzezhong <2264381489@qq.com>
2024-02-26 10:38:03 +0800

    add html response

--- http/responses.go
+++ http/responses.go
@@ -5,6 +5,7 @@ import (
 	"encoding/xml"
 	"fmt"
 	"net/http"
+	"reflect"
 
 	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
@@ -45,6 +46,53 @@ func doWriteXml(w http.ResponseWriter, code int, v any) error {
 	w.WriteHeader(code)
 
 	if n, err := w.Write(bs); err != nil {
+		// http.ErrHandlerTimeout has been handled by http.TimeoutHandler,
+		// so it's ignored here.
+		if err != http.ErrHandlerTimeout {
+			return fmt.Errorf("write response failed, error: %w", err)
+		}
+	} else if n < len(bs) {
+		return fmt.Errorf("actual bytes: %d, written bytes: %d", len(bs), n)
+	}
+
+	return nil
+}
+
+// OkHTML writes v into w with 200 OK.
+func OkHTML(w http.ResponseWriter, v any) {
+	WriteHTML(w, http.StatusOK, v)
+}
+
+// OkHTMLCtx writes v into w with 200 OK.
+func OkHTMLCtx(ctx context.Context, w http.ResponseWriter, v any) {
+	WriteHTMLCtx(ctx, w, http.StatusOK, v)
+}
+
+// WriteHTML writes v as HTML string into w with code.
+func WriteHTML(w http.ResponseWriter, code int, v any) {
+	if err := doWriteHTML(w, code, v); err != nil {
+		logx.Error(err)
+	}
+}
+
+// WriteHTMLCtx writes v as HTML string into w with code.
+func WriteHTMLCtx(ctx context.Context, w http.ResponseWriter, code int, v any) {
+	if err := doWriteHTML(w, code, v); err != nil {
+		logx.WithContext(ctx).Error(err)
+	}
+}
+
+func doWriteHTML(w http.ResponseWriter, code int, v any) error {
+	if reflect.TypeOf(v).Kind() != reflect.String {
+		return fmt.Errorf("the value must be a string, but got: %v", v)
+	}
+
+	bs := v.(string)
+
+	w.Header().Set(httpx.ContentType, HTMLContentType)
+	w.WriteHeader(code)
+
+	if n, err := w.Write([]byte(bs)); err != nil {
 		// http.ErrHandlerTimeout has been handled by http.TimeoutHandler,
 		// so it's ignored here.
 		if err != http.ErrHandlerTimeout {
